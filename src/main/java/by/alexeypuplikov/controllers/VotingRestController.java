9c2cf4f79ab6db8ec3146eb5014d2d82de0bcd1a
ivserhz <ivserhz@gmail.com>
2017-03-17 12:07:04 +0300

    Added ability to registration vote.

--- src/main/java/by/alexeypuplikov/controllers/VotingRestController.java
+++ src/main/java/by/alexeypuplikov/controllers/VotingRestController.java
@@ -1,22 +1,17 @@
 package by.alexeypuplikov.controllers;
 
+import by.alexeypuplikov.models.Vote;
 import by.alexeypuplikov.models.Voting;
 import by.alexeypuplikov.models.VotingOption;
 import by.alexeypuplikov.repositories.VoteRepository;
 import by.alexeypuplikov.repositories.VotingOptionRepository;
 import by.alexeypuplikov.repositories.VotingRepository;
 import by.alexeypuplikov.utils.ValidateVoting;
-import by.alexeypuplikov.utils.ValidateVotingOption;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -48,7 +43,7 @@ public class VotingRestController {
     @RequestMapping(method = RequestMethod.POST, value = "/{topic}/addVotingOption")
     public ResponseEntity<?> addVotingOption(@PathVariable String topic, @RequestBody VotingOption votingOption) {
         Voting voting = votingRepository.findByTopic(topic);
-        ValidateVotingOption.validateVotingOption(votingOptionRepository, votingOption.getOptionText(), voting);
+        ValidateVoting.validateVotingOption(votingOptionRepository, votingOption.getOptionText(), voting);
         votingOption.setVoting(voting);
         return new ResponseEntity<>(votingOptionRepository.save(votingOption), HttpStatus.CREATED);
     }
@@ -66,6 +61,22 @@ public class VotingRestController {
             voting.setLaunched(false);
         }
         return new ResponseEntity<>(votingRepository.save(voting), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/{topic}", params = "optionText")
+    public ResponseEntity<?> registrationVote(@PathVariable String topic, @RequestParam(value = "optionText") String optionText) {
+        Voting voting = votingRepository.findByTopic(topic);
+        ValidateVoting.validateRegistrationVote(votingOptionRepository, optionText, voting);
+        VotingOption votingOption = votingOptionRepository.findByVotingAndOptionText(voting, optionText);
+        Vote vote = new Vote(votingOption, voting);
+        return new ResponseEntity<>(voteRepository.save(vote), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{topic}")
+    public ResponseEntity<?> getVoting(@PathVariable String topic) {
+        //add validation for topic
+        Voting voting = votingRepository.findByTopic(topic);
+        return new ResponseEntity<>(voting, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/", produces = "application/json")
--- src/main/java/by/alexeypuplikov/utils/ValidateVoting.java
+++ src/main/java/by/alexeypuplikov/utils/ValidateVoting.java
@@ -2,6 +2,8 @@ package by.alexeypuplikov.utils;
 
 import by.alexeypuplikov.exception.VotingException;
 import by.alexeypuplikov.models.Voting;
+import by.alexeypuplikov.models.VotingOption;
+import by.alexeypuplikov.repositories.VotingOptionRepository;
 import by.alexeypuplikov.repositories.VotingRepository;
 
 public class ValidateVoting {
@@ -25,8 +27,30 @@ public class ValidateVoting {
         if (!changeState.equals(LAUNCH_STATE) && !changeState.equals(CLOSE_STATE)) {
             throw new VotingException(String.format("Parameter \"%s\" is incorrect!", changeState));
         }
-        if ((!voting.isLaunched() && changeState.equals(LAUNCH_STATE)) || (voting.isLaunched() && changeState.equals(CLOSE_STATE))) {
-            throw new VotingException("Voting is already have this state");
+        if ((voting.isLaunched() && changeState.equals(LAUNCH_STATE)) || (!voting.isLaunched() && changeState.equals(CLOSE_STATE))) {
+            throw new VotingException("Voting is already have this state.");
+        }
+    }
+
+    public static void validateVotingOption(VotingOptionRepository votingOptionRepository, String optionText, Voting voting) {
+        if (optionText.isEmpty()) {
+            throw new VotingException("Option text is empty.");
+        }
+        VotingOption votingOption = votingOptionRepository.findByVotingAndOptionText(voting, optionText);
+        if (votingOption != null) {
+            throw new VotingException(String.format("Option \"%s\" is already exist!", optionText));
+        }
+    }
+
+    public static void validateRegistrationVote(VotingOptionRepository votingOptionRepository, String optionText, Voting voting) {
+        if (!voting.isLaunched()) {
+            throw new VotingException("This voting is unavailable.");
+        }
+        if (optionText.isEmpty()) {
+            throw new VotingException("Make a choice!");
+        }
+        if (votingOptionRepository.findByVotingAndOptionText(voting, optionText) == null) {
+            throw new VotingException("This option is unavailable.");
         }
     }
 }
